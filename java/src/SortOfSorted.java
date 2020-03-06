184efa59bc9cd2601dbfe72a27e72c6892d75edb
madia <madisonanderson1@gmail.com>
2020-03-05 18:40:55 -0700

    redo entire thing and first test

--- java/src/SortOfSorted.java
+++ java/src/SortOfSorted.java
@@ -2,50 +2,39 @@ public class SortOfSorted {
 
     public static void main(String[] args) {
         int A[] = {1,8,5,7,2,6};
-        sortOfSorted(A);
+        sortOfSort(A);
     }
 
-    public static void sortOfSorted(int[] array){
-        int swing = 1;
-        int temp;
-        int maxIndex=0;
-        for(int i = 0; i < array.length-1; i++){
-
-            if(swing%2 == 0){
-                temp =
-                findMaxEvenIndex(swing, i, array);
-            } else {
-                temp = findMaxOddIndex(swing, i, array);
-                array[findMaxOddIndex(swing, i, array)]
-            }
-
-            if(i%2 != 0){
+    public static void sortOfSort(int[] array){
+        int swing =0;
+        int temp = 0;
+        int max= 0;
+        for(int i = 0; i < array.length; i++){
+            if(i%2 == 0){
                 swing++;
             }
-        }
-    }
-
-    public static int findMaxEvenIndex(int swing, int i, int[] array){
-        int max;
-
-            for (int j = swing-i; j < (array.length-(swing+1)); j++){
-                max =j;
-                if (array[j] > max){
-                    max = j;
+            if(swing%2 != 0){
+                max = swing - 1;
+                for(int j = swing - 1; j <= array.length-(i+2-swing);j++){
+                    if(array[j]>array[max]){
+                        max = j;
+                    }
                 }
-            }
-
-        return max;
-    }
-
-    public static int findMaxOddIndex(int swing, int i, int[] array){
-        int max = array[i];
-        for (int j = swing -1; j < array.length-j; j++){
-            if (array[j] > max){
-                max = j;
+                temp = array[array.length-(i+2-swing)];
+                array[array.length-(i+2-swing)] = array[max];
+                array[max] = temp;
+            } else {
+                max = i - swing;
+                for(int j = swing - 1; j < array.length-(swing);j++){
+                    if(array[j] > array[max]){
+                        max = j;
+                    }
+                }
+                temp = array[i - swing];
+                array[i - swing] = array[max];
+                array[max] = temp;
             }
         }
-        return max;
     }
 
 }
